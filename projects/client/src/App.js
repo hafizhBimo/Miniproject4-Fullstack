6e1d43bf5c103d60c2a4ed4984d606dc29729e85
hafizhbimo <hfzhbimo@gmail.com>
2023-07-12 23:14:10 +0700

    fixing init flowbite

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,12 +1,9 @@
-import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+
+import LoginPage from "./Page/LoginPage";
 
 function App() {
-  return (
-<div>halo ini siapa</div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
