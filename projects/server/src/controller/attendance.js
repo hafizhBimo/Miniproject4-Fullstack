099294c7a4a45653e5bda62819a1ee0d4695b032
hafizhbimo <hfzhbimo@gmail.com>
2023-07-15 19:06:42 +0700

    CLOCK IN FIX BANGET

--- projects/server/src/controller/attendance.js
+++ projects/server/src/controller/attendance.js
@@ -15,9 +15,8 @@ module.exports = {
         });
       }
 
-      //clock in
+      //format date
       const date = new Date();
-
       const timeNow = date.toLocaleTimeString([], {
         hour: "2-digit",
         minute: "2-digit",
@@ -35,7 +34,9 @@ module.exports = {
 
       //check clock in
       const checkClockIn = await db.Attendance.findOne({
-        where: { [db.Sequelize.Op.and]: [{ id: userId }, { date: dates }] },
+        where: {
+          [db.Sequelize.Op.and]: [{ user_id: userId }, { date: dates }],
+        },
       });
       if (checkClockIn) {
         return res.status(400).send({
@@ -44,7 +45,56 @@ module.exports = {
         });
       }
 
+      //clock in
       const tapIn = await db.Attendance.create({
+        user_id: userId,
+        clock_in: timeNow,
+        date: dates,
+        isValid: false,
+        createdAt: date,
+        updatedAt: date,
+      });
+
+      res.status(200).send({
+        message: "clock in success",
+        data: tapIn,
+      });
+    } catch (error) {
+      return res.status(400).send({
+        message: "fatal error",
+        error: error.message,
+      });
+    }
+  },
+  async clockOut(req, res) {
+    try {
+      //get user data
+      const userData = await db.User.findOne({
+        where: { id: userId },
+      });
+      //check if user exist
+      if (!userData) {
+        return res.status(404).send({
+          message: "invalid token",
+        });
+      }
+      //clock out
+      const date = new Date();
+
+      const timeNow = date.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+      const dateNow = date
+        .toLocaleDateString("zh-Hans-CN", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        })
+        .replace(/\//g, "-");
+
+      const tapOut = await db.Attendance.create({
         user_id: userId,
         clock_in: timeNow,
         date: dates,
@@ -57,11 +107,24 @@ module.exports = {
         message: "clock in success",
         data: tapIn,
       });
+      const dates = new Date(dateNow);
     } catch (error) {
-      return res.status(400).send({
-        message: "fatal error",
-        error: error.message,
+      return;
+    }
+  },
+  async employeeAttendanceReport(req, res) {
+    const userId = req.user.id;
+    try {
+      //get attendance data
+      const attendaceData = await db.Attendance.findAll({
+        where: { user_id: userId },
       });
+      res.status(200).send({
+        message: "data successfully retrieved",
+        data: attendaceData,
+      });
+    } catch (error) {
+      return;
     }
   },
 };
